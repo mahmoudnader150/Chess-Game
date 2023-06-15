807b99ba974e12b44f2ac5aada074092c89ed7af
mahmoudnader150 <91666764+mahmoudnader150@users.noreply.github.com>
2023-06-15 19:33:43 +0300

    start main loop

---
+++ src/game.py
@@ -0,0 +1,1 @@
+# This file is responsible for rendering the game
--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
+import pygame
+import sys
+
+from const import *
+
+
 class Main:
 
     def __init__(self):
-        pass
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Chess')
 
     def mainloop(self):
-        pass
+
+        while True:
+            #   Quiting the application
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
 
 
 main = Main()
