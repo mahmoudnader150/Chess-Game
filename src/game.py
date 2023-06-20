9f9e1174a5f6aad9d8a344f7ee3a4c63224757ba
mahmoudnader150 <91666764+mahmoudnader150@users.noreply.github.com>
2023-06-20 21:52:02 +0300

    update game file with themes

--- src/game.py
+++ src/game.py
@@ -4,6 +4,7 @@ import pygame
 from const import *
 from board import Board
 from dragger import Dragger
+from config import Config
 
 
 class Game:
@@ -12,16 +13,18 @@ class Game:
         self.hovered_sqr = None
         self.board = Board()
         self.dragger = Dragger()
-
+        self.config = Config()
     # show methods
 
     def show_bg(self, surface):
+
+        theme = self.config.theme
+
         for row in range(ROWS):
             for col in range(COLS):
-                if (row+col) % 2 == 0:
-                    color = (234, 235, 200)
-                else:
-                    color = (119, 154, 88)
+                # color
+                color = theme.bg.light if(row+col) % 2 == 0 else theme.bg.dark
+
                 # rect x,y,h,w
                 rect = (col*SQSIZE, row*SQSIZE, SQSIZE, SQSIZE)
 
@@ -44,14 +47,16 @@ class Game:
                         surface.blit(img, piece.texture_rect)
 
     def show_moves(self, surface):
+        theme = self.config.theme
+
         if self.dragger.dragging:
             piece = self.dragger.piece
 
             # Loop all valid moves
             for move in piece.moves:
                 # color
-                color = '#C86464' if (
-                    move.final.row + move.final.col) % 2 == 0 else '#C84646'
+                color = theme.moves.light if (
+                    move.final.row + move.final.col) % 2 == 0 else theme.moves.dark
                 # rect
                 rect = (move.final.col*SQSIZE,
                         move.final.row*SQSIZE, SQSIZE, SQSIZE)
@@ -59,14 +64,15 @@ class Game:
                 pygame.draw.rect(surface, color, rect)
 
     def show_last_move(self, surface):
+        theme = self.config.theme
         if self.board.last_move:
             initial = self.board.last_move.initial
             final = self.board.last_move.final
 
             for pos in [initial, final]:
                 # color
-                color = (244, 247, 116) if (
-                    pos.row + pos.col) % 2 == 0 else (172, 195, 51)
+                color = theme.trace.light if (
+                    pos.row + pos.col) % 2 == 0 else theme.trace.dark
                 # rect
                 rect = (pos.col*SQSIZE, pos.row*SQSIZE, SQSIZE, SQSIZE)
                 # blit
